89c193f95c69f61af96a98217a96e0234aefc73c
Aishwarya Pai <aishwaryampai200203@gmail.com>
2022-01-14 18:51:40 +0530

    Added Some ScreenShots

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
-const newYears = '1 Jan 2022';
+const newYears = '13 Sep 2022';
 
-function countdown(){
-    const newYearsDate =new Date(newYears);
-    const currentDate =new Date();
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
 
-    const totalSeconds =(newYearsDate -currentDate)/1000;
-    const days =Math.floor(totalSeconds/3600/24);
-    const hours=Math.floor(totalSeconds/3600)%24;
-    const mins=Math.floor(totalSeconds/60)%60;
-    const seconds=Math.floor(totalSeconds)%60;
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
 
-daysEl.innerHTML= formatTime(days);
-hoursEl.innerHTML= formatTime(hours);
-minsEl.innerHTML= formatTime(mins);
-secondsEl.innerHTML= formatTime(seconds);
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
-function formatTime(time){
-    return time<10 ?(`0${time}`):time;
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
 }
 // console.log(days,hours,mins,seconds);
 
